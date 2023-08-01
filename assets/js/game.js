ce9c9375ca3d4c2bc08259d557fa0cc80c730356
Craig-Hudson <craighudson1211@gmail.com>
2023-08-01 22:34:01 +0100

    add enable and disable button functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,14 +7,14 @@ const hardQuestions = ('https://opentdb.com/api.php?amount=5&category=9&difficul
 
 let data = {};
 let score = 0;
-// let questionIndex = 1;
+// let questionIndex = 0;
+let questionIndex = 1;
 console.log(data);
 
 async function callApi(urlApi) {
   const response = await fetch(urlApi);
   if (response.status >= 200 && response.status <= 299) {
     data = await response.json();
-    
     getQuestion(data);
     getAnswers(data);
   } else 
@@ -26,39 +26,41 @@ function getQuestion(data) {
   console.log(data);
   console.log(data.results);
   const question = document.getElementById('question').innerHTML = data.results[0].question;
-  
 }
+
 //get incorrect and correct answers, and store them randomly so the correct answer is not at the same index for every question
 function getAnswers(data) {
-    const correctAnswer = data.results[0].correct_answer;
-    const incorrectAnswer = data.results[0].incorrect_answers;
-    const answerButtons = document.querySelectorAll('.answer-button');
-    const newAnswerArray = incorrectAnswer.concat(correctAnswer);
-    //call shuffle answers function so correct answer appears at different indexes for every question
-    shuffleAnswers(newAnswerArray);
+  
+  const correctAnswer = data.results[0].correct_answer;
+  const incorrectAnswer = data.results[0].incorrect_answers;
+  const answerButtons = document.querySelectorAll('.answer-button');
+  const newAnswerArray = incorrectAnswer.concat(correctAnswer);
+  //call shuffle answers function so correct answer appears at different indexes for every question
+  shuffleAnswers(newAnswerArray);
 
-    for(let i = 0; i < newAnswerArray.length; i++) {
-      answerButtons[i].innerHTML = newAnswerArray[i];
-      // event listener to determine the correct answer
-      answerButtons[i].addEventListener('click', function () {
+  for(let i = 0; i < newAnswerArray.length; i++) {
+    answerButtons[i].innerHTML = newAnswerArray[i];
+    // event listener to determine the correct answer
+    answerButtons[i].addEventListener('click', function () {
         
-        
-        if(this.innerHTML === correctAnswer) {
-          // alert('correct Answer')
-          this.style.backgroundColor = 'orange';
-          for(let j = 0; j < answerButtons.length; j++) {
-            answerButtons[j].disabled = true;
-          }
-          // nextQuestion();
-          } else {
-          // alert('incorrect Answer');
-          this.style.backgroundColor = 'red';
-          for(let j = 0; j < answerButtons.length; j++) {
-            answerButtons[j].disabled = true;
-          }
+      if(this.innerHTML === correctAnswer) {
+      // alert('correct Answer')
+      this.style.backgroundColor = 'orange';
+      for(let j = 0; j < answerButtons.length; j++) {
+        answerButtons[j].disabled = true;
+      }
+      
+      enableNextButton();
+      
+      } else {
+      // alert('incorrect Answer');
+      this.style.backgroundColor = 'red';
+      for(let j = 0; j < answerButtons.length; j++) {
+        answerButtons[j].disabled = true;
         }
-      })
-    }
+      }
+    })
+  }
     console.log(correctAnswer);
     console.log(newAnswerArray);
     // shuffleAnswers(newAnswerArray);
@@ -76,38 +78,49 @@ function shuffleAnswers(array) {
 
 console.log('working')
 
-let nextButton = document.getElementById('next-button').addEventListener('click',nextQuestion)
+// let nextButton = document.getElementById('next-button').addEventListener('click',nextQuestion)
 
 
-// Function to handle the next button click event
-// function nextQuestion() {
-//   score += 1; // Increase the score
-//   let questionIndex = questionIndex+= 1; // Increment the question index
+//Function to handle the next button click event
+function nextQuestion() {
+  score ++;
+  const question = data.results[questionIndex].question;
+  const correctAnswer = data.results[questionIndex].correct_answer;
+  const incorrectAnswers = data.results[questionIndex].incorrect_answers;
+  const answerButtons = document.querySelectorAll('.answer-button');
+  const newAnswerArray = incorrectAnswers.concat(correctAnswer);
+  shuffleAnswers(newAnswerArray);
+  console.log(question)
+  document.getElementById('question').innerHTML = question;
+  for (let i = 0; i < newAnswerArray.length; i++) {
+    answerButtons[i].innerHTML = newAnswerArray[i];
+    answerButtons[i].style.backgroundColor = '';
+    answerButtons[i].disabled = false;
+  }
 
-//   // Check if there are more questions based on the current question index
-//   if (questionIndex > data.results.length) {
-//     // Display the final score or redirect to a different page
-//     // You can modify this section based on your requirements
-//     alert(`Quiz complete! Your score: ${score}/${data.results.length}`);
-//     return;
-//   }
+  questionIndex ++;
+  console.log(question);
+  if (questionIndex >= data.results.length) {
+    alert(`Quiz complete! Your score: ${score}/${data.results.length}`);
+    return;
+  }
+  
+  disableNextButton();
+}
 
-//   // Get the new question and answers
-//   const question = data.results[questionIndex - 1].question;
-//   const correctAnswer = data.results[questionIndex - 1].correct_answer;
-//   const incorrectAnswer = data.results[questionIndex - 1].incorrect_answers;
-//   const answerButtons = document.querySelectorAll('.answer-button');
-//   const newAnswerArray = incorrectAnswer.concat(correctAnswer);
-//   shuffleAnswers(newAnswerArray);
+//display next button and call next question function when button is clicked
+function enableNextButton() {
+  const nextButton = document.getElementById('next-button');
+  nextButton.style.display = 'block'
+}
 
-//   // Update the question display and answer buttons
-//   document.getElementById('question').innerHTML = question;
-//   for (let i = 0; i < newAnswerArray.length; i++) {
-//     answerButtons[i].innerHTML = newAnswerArray[i];
-//     answerButtons[i].style.backgroundColor = ''; // Reset button color
-//     answerButtons[i].disabled = false; // Enable answer buttons
-//   }
-// }
+document.getElementById('next-button').addEventListener('click', nextQuestion);
+
+function disableNextButton() {
+  const nextButton = document.getElementById('next-button');
+  nextButton.style.display = 'none';
+}
+
 
 callApi(easyQuestions);
 // getQuestion(data);

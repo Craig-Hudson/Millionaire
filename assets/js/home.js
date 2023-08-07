481952c055c956cbc70387f16b81079c4a2e2c62
Craig-Hudson <craighudson1211@gmail.com>
2023-08-07 20:19:13 +0100

    FIX bug in closing modals, more will be documented in readme

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,16 +1,15 @@
 'use strict';
-// to open and close modal on how to play, highscores, and contact us buttons
+
 document.addEventListener('DOMContentLoaded', () => {
   updateHighScores();
 });
 
 const openBtnModals = document.querySelectorAll('.open-modal');
 const modalContainer = document.querySelector('.modal-container');
-const modals = document.querySelectorAll('.modals')
-const closeModals = document.querySelectorAll('.close-modal')
-// iterate over the 3 modals to determine the data modal
+const closeModals = document.querySelectorAll('.close-modal');
 
- openBtnModals.forEach((btn) => {
+// iterate over the 3 modals to determine the data modal
+openBtnModals.forEach((btn) => {
   btn.addEventListener('click', function() {
     const modalId = this.getAttribute('data-modal');
     const modal = document.getElementById(modalId);
@@ -19,37 +18,40 @@ const closeModals = document.querySelectorAll('.close-modal')
   });
 });
 
-// function to close modals
-
+// function to close individual modals and by remove active classes to container and individual meodal
 function closeModal() {
+  const modalContainer = document.querySelector('.modal-container');
   modalContainer.classList.remove('active');
-  modals.forEach((modals) => {
-    modals.classList.remove('active');
+
+  const modals = document.querySelectorAll('.modal.active');
+  modals.forEach((modal) => {
+    modal.classList.remove('active');
   });
 }
-// to close modals when user clicks close / x
+
+
+// close modals when user clicks the x or close
 closeModals.forEach((btn) => {
-  btn.addEventListener('click', closeModal)
-})
+  btn.addEventListener('click', closeModal);
+});
 
-// when user clicks anywhere on the page the modal will close
-
+// close modals when user clicks anywhere on screen
 modalContainer.addEventListener('click', function(event) {
-  if(event.target === modalContainer) {
+  if (event.target === modalContainer) {
     closeModal();
   }
-})
+});
 
-//  When user presses escape key the modal will close
 
+// close modals when user presses escape key
 document.addEventListener('keydown', function(event) {
   const key = event.key;
-  if(key === "Escape") {
+  if (key === 'Escape') {
     closeModal();
   }
-})
+});
 
-// When user clicks start quiz, take them to quiz page
+// To take user to quiz page
 function newPageQuiz() {
   window.location.href = 'quiz.html';
 }
@@ -59,7 +61,6 @@ quizHtmlPage.addEventListener('click', function() {
   setTimeout(newPageQuiz, 500);
 });
 
-
 // https://www.youtube.com/watch?v=jfOv18lCMmw&list=PLDlWc9AfQBfZIkdVaOQXi1tizJeNJipEx&index=10
 // tutorial link above to help implement and make my own adjustments
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
@@ -68,12 +69,12 @@ const highScoresList = document.getElementById('highscores-list');
 
 function updateHighScores () {
   // Clear previously displayed high scores
-  highScoresList.innerHTML = '';
+  // highScoresList.innerHTML = '';
 
   // Iterate over the high scores array and generate HTML list items
   highScores.forEach((score) => {
     const listItem = document.createElement('li');
-    listItem.innerText = `${score.userName} ${score.score}`;
+    listItem.innerText = `${score.userName} - ${score.score}`;
     highScoresList.appendChild(listItem);
   });
 }

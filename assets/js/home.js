50aba5ba80ea2b55ff6af0f6dff388331d0fcd0a
Craig-Hudson <craighudson1211@gmail.com>
2023-08-16 23:44:16 +0100

    create: function to keep the event listeners in for modals

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,26 +1,22 @@
-// 'use strict';
-// window.onload = function () {
-  // localStorage.clear();
-// }
 const openBtnModals = document.querySelectorAll('.open-modal');
 const modalContainer = document.querySelector('.modal-container');
 const closeModals = document.querySelectorAll('.close-modal');
-// localStorage.clear();
-// iterate over the 3 modals to determine the data modal
-openBtnModals.forEach((btn) => {
-  btn.addEventListener('click', function() {
-    const modalId = this.getAttribute('data-modal');
-    const modal = document.getElementById(modalId);
-    modalContainer.classList.add('active');
-    modal.classList.add('active');
-    if (modalId === 'high-scores') {
-      console.log('highscores')
-      updateHighScores();
-    }
-  });
-});
+const quizHtmlPage = document.querySelector('.start-quiz');
+const highScoresList = document.getElementById('highscores-list');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-// function to close individual modals and by remove active classes to container and individual meodal
+// Function to open a modal
+function openModal (modalId) {
+  const modal = document.getElementById(modalId);
+  modalContainer.classList.add('active');
+  modal.classList.add('active');
+  if (modalId === 'high-scores') {
+    console.log('highscores');
+    updateHighScoresDisplay();
+  }
+}
+
+// Function to close individual modals and remove active classes
 function closeModal() {
   const modalContainer = document.querySelector('.modal-container');
   modalContainer.classList.remove('active');
@@ -31,48 +27,56 @@ function closeModal() {
   });
 }
 
-// close modals when user clicks the x or close
-closeModals.forEach((btn) => {
-  btn.addEventListener('click', closeModal);
-});
-
-// close modals when user clicks anywhere on screen
-modalContainer.addEventListener('click', function(event) {
+// Function to close modals when user clicks anywhere on screen
+function closeModalsOnOutsideClick(event) {
   if (event.target === modalContainer) {
     closeModal();
   }
-});
+}
 
-// close modals when user presses escape key
-document.addEventListener('keydown', function(event) {
+// Function to close modals when user presses the escape key
+function closeModalsOnEscapeKey(event) {
   const key = event.key;
   if (key === 'Escape') {
     closeModal();
   }
-});
+}
 
-// To take user to quiz page
+// Function to navigate to the quiz page
 function newPageQuiz() {
   window.location.href = 'quiz.html';
 }
 
-const quizHtmlPage = document.querySelector('.start-quiz');
-quizHtmlPage.addEventListener('click', function() {
-  setTimeout(newPageQuiz, 500);
-});
-
-// https://www.youtube.com/watch?v=jfOv18lCMmw&list=PLDlWc9AfQBfZIkdVaOQXi1tizJeNJipEx&index=10
-// tutorial link above to help implement and make my own adjustments
-const highScoresList = document.getElementById('highscores-list');
-// Get high scores from local storage
-const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-
-function updateHighScores() {
+// Function to update high scores display
+function updateHighScoresDisplay() {
   highScoresList.innerHTML = '';
- // Display the sorted high scores in the HTML
   highScores.forEach(score => {
     const listItem = document.createElement('li');
     listItem.innerText = score.userName + ' - ' + '£' + score.score;
     highScoresList.appendChild(listItem);
   });
 }
+
+// Function to set up event listeners
+function setupEventListeners() {
+  openBtnModals.forEach((btn) => {
+    btn.addEventListener('click', function() {
+      const modalId = this.getAttribute('data-modal');
+      openModal(modalId);
+    });
+  });
+
+  closeModals.forEach((btn) => {
+    btn.addEventListener('click', closeModal);
+  });
+
+  modalContainer.addEventListener('click', closeModalsOnOutsideClick);
+
+  document.addEventListener('keydown', closeModalsOnEscapeKey);
+
+  quizHtmlPage.addEventListener('click', function() {
+    setTimeout(newPageQuiz, 500);
+  });
+}
+setupEventListeners();
+updateHighScoresDisplay();

c620b3867ac446ae2bddd7f48b1a4d45ba598175
Craig-Hudson <craighudson1211@gmail.com>
2023-08-19 17:29:54 +0100

    add: event listener so user can return back to home page

---
+++ assets/js/thankyou.js
@@ -0,0 +1,4 @@
+//  When you clicks return home button it will send user back to home page
+document.querySelector('.button-thankyou').addEventListener('click', () => {
+  window.location.href = 'index.html'
+})
